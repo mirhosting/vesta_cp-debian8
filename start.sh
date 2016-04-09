c47954732645f01e2907fad4875dd1b2d0eb2439
deadsik <deadsik@ukr.net>
2016-04-09 04:26:00 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sleep 10;
 iptables -t nat --flush;
 OLDIP=$(cat /root/oldip.txt);
 NEWIP=$(ifconfig | grep 'inet addr:'| grep -v '127.0.0.1' | grep -v 'P-t-P:10.' | cut -d: -f2 | awk '{ print $1}');
