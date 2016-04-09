a5a2ec626f861a6bf82571b9139fd7e6ec772674
deadsik <deadsik@ukr.net>
2016-04-09 04:23:12 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 iptables -t nat --flush;
-IP=$(ifconfig | grep 'inet addr:'| grep -v '127.0.0.1' | grep -v 'P-t-P:10.' | cut -d: -f2 | awk '{ print $1}')
-echo "$IP" > /root/ip.txt;
+OLDIP=$(cat /root/oldip.txt);
+NEWIP=$(ifconfig | grep 'inet addr:'| grep -v '127.0.0.1' | grep -v 'P-t-P:10.' | cut -d: -f2 | awk '{ print $1}');
+
+sed -i "s/$OLDIP/$NEWIP/g" /etc/apache2/mods-enabled/rpaf.conf;
+sed -i "s/$OLDIP/$NEWIP/g" /etc/apache2/conf.d/$OLDIP.conf;
+/etc/init.d/apache2 restart;
+sed -i "s/$OLDIP/$NEWIP/g" /etc/nginx/conf.d/$OLDIP.conf;
+sed -i "s/$OLDIP/$NEWIP/g" /home/admin/conf/web/nginx.conf;
+/etc/init.d/nginx restart;
