79fb9674dc2ab98c216b1273e72d2f3c43edd50a
deadsik <deadsik@ukr.net>
2016-04-09 03:34:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-8} > pass.txt;
 pass=$(cat pass.txt);
-echo $pass;
+cd /root && yes | bash vst-install.sh --nginx yes --apache yes --phpfpm no --vsftpd no  --proftpd yes --exim yes --dovecot yes --spamassassin no --clamav no  --named yes --iptables yes --fail2ban yes --mysql yes --postgresql no  --remi yes --quota no --hostname vesta.host --email admin@example.com  --password $pass;
