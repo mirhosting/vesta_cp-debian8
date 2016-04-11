b4247344f869cba5a608c0b5c11a708b990f9de1
deadsik <deadsik@ukr.net>
2016-04-11 23:16:28 +0300

    Update install_mysql.sh

--- install_mysql.sh
+++ install_mysql.sh
@@ -2,3 +2,4 @@
 echo "deb http://repo.mysql.com/apt/debian/ jessie mysql-5.6" >> /etc/apt/sources.list;
 echo "deb-src http://repo.mysql.com/apt/debian/ jessie mysql-5.6" >>  /etc/apt/sources.list;
 apt-get -y update;
+yes | apt-get install mysql-server-5.6 mysql-client-5.6 mysql-client-core-5.6;
