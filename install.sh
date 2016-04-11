2d22f89336a4bb5d61e8993c58a40a3357123863
deadsik <deadsik@ukr.net>
2016-04-11 15:28:49 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-#что-то не сработало...
-#OLDIP=$(ifconfig | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}');
-#echo "$OLDIP" > /root/oldip.txt;
-#сделал так:
 echo "172.17.0.3" > /root/oldip.txt;
-
 < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-8} > /root/pass.txt;
 pass=$(cat /root/pass.txt);
 cd /root && yes | bash vst-install.sh --nginx yes --apache yes --phpfpm no --vsftpd no  --proftpd yes --exim yes --dovecot yes --spamassassin no --clamav no  --named yes --iptables yes --fail2ban yes --mysql yes --postgresql no  --remi yes --quota no --hostname vesta.host --email admin@example.com  --password $pass;
