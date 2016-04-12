57e4e0f452470b4b5485a3d7aa1c6aec6fddb0f6
deadsik <deadsik@ukr.net>
2016-04-12 13:45:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 echo "172.17.0.3" > /root/oldip.txt;
 < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-8} > /root/pass.txt;
 pass=$(cat /root/pass.txt);
-cd /root && yes | bash vst-install.sh --nginx yes --apache yes --phpfpm no --vsftpd no  --proftpd yes --exim yes --dovecot yes --spamassassin no --clamav no  --named yes --iptables yes --fail2ban yes --mysql yes --postgresql no  --remi yes --quota no --hostname example.com --email admin@example.com  --password $pass;
+#cd /root && yes | bash vst-install.sh --nginx yes --apache yes --phpfpm no --vsftpd no  --proftpd yes --exim yes --dovecot yes --spamassassin no --clamav no  --named yes --iptables yes --fail2ban yes --mysql yes --postgresql no  --remi yes --quota no --hostname example.com --email admin@example.com  --password $pass;
+cd /root && yes | bash vst-install.sh --nginx yes --apache yes --phpfpm no --vsftpd no  --proftpd yes --exim yes --dovecot yes --spamassassin no --clamav no  --named yes --iptables yes --fail2ban yes --mysql no --postgresql no  --remi yes --quota no --hostname example.com --email admin@example.com  --password $pass;
