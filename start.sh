2089cfe23edb8dd6670b98fe995697fc419e29f8
mirhosting <dev@mirhosting.com>
2017-06-13 23:34:33 +0200

    Create start.sh

--- start.sh
+++ start.sh
@@ -11,9 +11,13 @@ NEWIP=$(ifconfig | grep 'inet addr:'| grep -v '127.0.0.1' | grep -v 'P-t-P:10.' 
 killall -9 apache2;
 killall -9 nginx;
 cd /usr/local/vesta/data/ips && mv * $NEWIP;
-cd /etc/apache2/conf.d && sed -i -- 's/172.*.*.*:80/$NEWIP:80/g' * && sed -i -- 's/172.*.*.*:8443/$NEWIP:8443/g' *;
-cd /etc/nginx/conf.d && sed -i -- 's/172.*.*.*:80;/80;/g' * && sed -i -- 's/172.*.*.*:8080/$NEWIP:8080/g' *;
-cd /home/admin/conf/web && sed -i -- 's/172.*.*.*:80;/80;/g' * && sed -i -- 's/172.*.*.*:8080/$NEWIP:8080/g' *;
+cd /etc/apache2/conf.d && sed -i -- "s/172.*.*.*:80/$NEWIP:80/g" * && sed -i -- "s/172.*.*.*:8443/$NEWIP:8443/g" *;
+cd /etc/nginx/conf.d && sed -i -- "s/172.*.*.*:80;/80;/g" * && sed -i -- "s/172.*.*.*:8080/$NEWIP:8080/g" *;
+cd /home/admin/conf/web && sed -i -- "s/172.*.*.*:80;/80;/g" * && sed -i -- "s/172.*.*.*:8080/$NEWIP:8080/g" *;
+
+sed -i 's/eth0/venet0/g' /usr/local/vesta/data/ips/$NEWIP;
+sed -i 's/255.255.0.0/255.255.255.255/g' /usr/local/vesta/data/ips/$NEWIP;
+sed -i 's/52.206.84.71//g' /usr/local/vesta/data/ips/$NEWIP;
 
 mv /etc/apache2/conf.d/$OLDIP.conf /etc/apache2/conf.d/$NEWIP.conf
 mv /etc/nginx/conf.d/$OLDIP.conf /etc/nginx/conf.d/$NEWIP.conf
